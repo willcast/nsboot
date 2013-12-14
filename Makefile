09f5cecba7cdc568c549b500d61e1e4f3a083112
William Castro <castrwilliam@gmail.com>
2013-12-14 01:46:36 -0700

    enhance top-of-file comments

--- Makefile
+++ Makefile
@@ -1,4 +1,22 @@
-# makefile 2.0 for nsboot
+#	nsboot/Makefile
+#	Makefile for nsboot
+#
+#	Copyright (C) 2013 Will Castro
+#
+#	This file is part of nsboot.
+#
+#	nsboot is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 2 of the License, or
+#	(at your option) any later version.
+#
+#	nsboot is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with nsboot.  If not, see <http://www.gnu.org/licenses/>
 
 CC := gcc
 CFLAGS := -D_GNU_SOURCE -std=gnu99 -Os -Wall
--- lvset.h
+++ lvset.h
@@ -1,20 +1,23 @@
 /*
-    Copyright (C) 2013 Will Castro
+	nsboot/lvset.c
+	header for Logical Volume Set related functions
 
-    This file is part of nsboot.
+	Copyright (C) 2013 Will Castro
 
-    nsboot is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 2 of the License, or
-    (at your option) any later version.
+	This file is part of nsboot.
 
-    nsboot is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	nsboot is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with nsboot.  If not, see <http://www.gnu.org/licenses/>
+	nsboot is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with nsboot.  If not, see <http://www.gnu.org/licenses/>
 */
 
 #ifndef NSBOOT_LVSET_H
